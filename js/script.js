2602da0b9e0f5583b4f59a6cca16d98a95a72ab2
Sahid Gutierrez <s.gutierrez@ihsph.org>
2018-12-20 23:01:43 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -9,6 +9,42 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
     });
+function name(answer){
+    if (answer === "Red") {
+        return "4" ;
+    }
+     else if (answer === "Pink") {
+         return "2";
+    } else if (answer === "Brown"){
+        return "3";
+    } else if (answer === "Black"){
+        return "1";
+    }
+ 
+function question1(answer){
+    if (answer === "") {
+        return "4" ;
+    }
+     else if (answer === "") {
+         return "3";
+    } else if (answer === ""){
+        return "2";
+    } else if (answer === ""){
+        return "1";
+    }
+
+function question2(answer){
+    if (answer === "") {
+        return "4" ;
+    }
+     else if (answer === "") {
+         return "3";
+    } else if (answer === ""){
+        return "2";
+    } else if (answer === ""){
+        return "1";
+    }
 
 });
